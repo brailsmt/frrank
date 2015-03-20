93153e22881c6e01401a22becdad9cc9bc85ba04
Michael Brailsford <michael.brailsford@cerner.com>
2015-03-20 08:23:56 -0500

    Connect to FRC and print a static web page

--- project.clj
+++ project.clj
@@ -1,6 +1,10 @@
 (defproject frrank "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "FRRank:  Display FIRST Robotics Rankings"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [clj-http "1.0.1"]
+                 [compojure "1.3.2"]
+                 [hiccup "1.0.5"]]
+  :main frrank.core)
--- src/frrank/core.clj
+++ src/frrank/core.clj
@@ -1,6 +1,35 @@
-(ns frrank.core)
+(ns frrank.core
+  (:require 
+    [clojure.string :as str]
+    [clj-http.client :as client]
+    [compojure.route :as route]
+    [hiccup.core :as hc]
+    [hiccup.page :as page]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn build-url [& parts] (str/join "/" parts))
+
+(def api-root "api/v1.0")
+(def staging-server "https://frc-staging-api.usfirst.org")
+(def prod-server  "https://frc-api.usfirst.org")
+(def auth-token "Basic YnJhaWxzbXQ6QTA2RjlERDAtNTI4OC00OTBGLUJFMDktQjM2MzRDMjlBOTJDCg==")
+
+(defn team-ranking 
+  [server year]
+  (let [response (client/get (build-url [server api-root server year]) {:debug true :headers { :authorization auth-token }})]
+  ))
+
+
+(defn frrank 
+  [server year]
+  (let [response (client/get (build-url server api-root year) {:debug true :headers { :authorization auth-token }})]
+    (page/html5 [:body [:h1 (str "FRC " year "!!!")] [:pre '(response)]])))
+
+(defn -main 
+  [& args]
+  (let [page (frrank staging-server 2015)]
+    (println page)
+    (spit "out.html" page)))
+
+
+;(defroutes routes
+  ;GET "/" [] (team-ranking))
