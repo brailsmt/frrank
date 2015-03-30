11d3696cdc81e969a953f7e01e78da71261f58ad
Michael Brailsford <michael.brailsford@cerner.com>
2015-03-30 18:35:09 -0500

    Adding the beginnings of some SQL to store the data retrieved from the FRC API

--- project.clj
+++ project.clj
@@ -4,7 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/java.jdbc "0.3.6"]
+                 [mysql/mysql-connector-java "5.1.25"]
                  [clj-http "1.0.1"]
                  [compojure "1.3.2"]
+                 [cheshire "5.4.0"]
                  [hiccup "1.0.5"]]
   :main frrank.core)
--- src/frrank/core.clj
+++ src/frrank/core.clj
@@ -3,6 +3,9 @@
     [clojure.string :as str]
     [clj-http.client :as client]
     [compojure.route :as route]
+    [clojure.java.jdbc :as jdbc]
+    [clojure.pprint :as pp]
+    [cheshire.core :refer :all]
     [hiccup.core :as hc]
     [hiccup.page :as page]))
 
@@ -13,23 +16,79 @@
 (def prod-server  "https://frc-api.usfirst.org")
 (def auth-token "Basic YnJhaWxzbXQ6QTA2RjlERDAtNTI4OC00OTBGLUJFMDktQjM2MzRDMjlBOTJDCg==")
 
-(defn team-ranking 
+(def mysql-db {:subprotocol "mysql"
+               :subname "//127.0.0.1:3306/frrank"
+               :user "frrank"
+               :password "frrank"})
+
+(defn create-season-tbl
+  "Create the season table"
+  []
+  (jdbc/create-table
+    :season
+    [:year :integer "primary key"]
+    [:frcChampionship :datetime]
+    [:eventCount :integer]
+    [:gameName "varchar(128)"]
+    [:kickoff :datetime]
+    [:rookieStart :integer]
+    [:teamCount :integer]))
+
+(defn create-teams-tbl
+  "Create the teams table"
+  []
+  (jdbc/create-table
+    :teams
+    [:teamNumber :integer "primary key"]
+    [:rookieYear :integer]
+    [:stateProv "varchar(10)"]
+    [:robotName "varchar(256)"]
+    [:city "varchar(50)"]
+    [:nameFull "varchar(1024)"]
+    [:nameShort "varchar(256)"]
+    [:districtCode "varchar(16)"]
+    [:country "varchar(32)"]))
+
+(defn create-database
+  [dbconn]
+  (jdbc/with-connection dbconn (jdbc/transaction 
+      (create-season-tbl)
+      (create-teams-tbl)
+      )))
+
+
+(defn- parse-json [json] (parse-string json (fn [k] (keyword k))))
+(def year-info 
+  (let [response (client/get (build-url staging-server api-root "2015") {:headers { :authorization auth-token }})]
+        (assoc (parse-json (:body response)) :full response)))
+
+(def team-listing (atom {}))
+
+(defn team-list
   [server year]
-  (let [response (client/get (build-url [server api-root server year]) {:debug true :headers { :authorization auth-token }})]
-  ))
+  (let [page-range (map inc (range (/ (:teamCount year-info) 50)))
+        base-url (str (build-url server api-root "teams" year) "?page=")
+        tlist []
+        full-list (flatten (map #(cons tlist (:teams (parse-json (:body (client/get (str base-url %) {:debug true :headers { :authorization auth-token }}))))) page-range))]
+    (swap! (map 
 
-
-(defn frrank 
-  [server year]
-  (let [response (client/get (build-url server api-root year) {:debug true :headers { :authorization auth-token }})]
-    (page/html5 [:body [:h1 (str "FRC " year "!!!")] [:pre '(response)]])))
-
-(defn -main 
-  [& args]
-  (let [page (frrank staging-server 2015)]
-    (println page)
-    (spit "out.html" page)))
-
-
-;(defroutes routes
-  ;GET "/" [] (team-ranking))
+;
+;(defn team-ranking 
+;  [server year]
+;  (let [response (client/get (build-url server api-root server year) {:debug true :headers { :authorization auth-token }})]))
+;
+;
+;(defn frrank 
+;  [server year]
+;  (let [nteams (:teamCount year-info)]
+;    (prn nteams)))
+;
+;(defn -main 
+;  [& args]
+;  (let [page (frrank staging-server 2015)]
+;    (println page)
+;    (spit "out.html" page)))
+;
+;
+;;(defroutes routes
+;  ;GET "/" [] (team-ranking))
