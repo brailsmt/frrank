fd54fed2c1b9298b4134785ee22417964a060ee7
Michael Brailsford <michael.brailsford@cerner.com>
2015-04-27 09:39:51 -0500

    Code used for FRC World Championships 2015

--- src/frrank/core.clj
+++ src/frrank/core.clj
@@ -18,6 +18,9 @@
 (def prod-server  "https://frc-api.usfirst.org")
 (def auth-token "Basic YnJhaWxzbXQ6QTA2RjlERDAtNTI4OC00OTBGLUJFMDktQjM2MzRDMjlBOTJDCg==")
 
+;(def server staging-server)
+(def server prod-server)
+
 (def db-spec {:classname "com.mysql.jdbc.Driver"
               :subprotocol "mysql"
               :subname "//127.0.0.1:3306/frrank"
@@ -26,13 +29,17 @@
 (defn- parse-json [json] (parse-string json #(keyword (str/lower-case %))))
 
 (def current-year (.getYear (org.joda.time.DateTime.)))
+(def std-header {:debug true :headers { :authorization auth-token }})
 
-(defn get-or-load-season-info
+;(defn http-query
+  ;[url]
+
+(defn get-season-info
   "Queries the frrank database for season information, or queries it from FIRST and populates the table"
   [] ; parameterize this with year
   (if-let [data-from-dbms (seq (jdbc/query db-spec ["select * from season where year = ?" current-year]))]
     (first data-from-dbms)
-    (let [{:keys [body]}                                  (client/get (build-url staging-server api-root "2015") {:headers { :authorization auth-token }})
+    (let [{:keys [body]}                                  (client/get (build-url server api-root "2015") std-header)
           {:keys [kickoff frcchampionship] :as year-info} (parse-json body)
           kickoffdt (dtfmt/parse (dtfmt/formatters :date-time-no-ms) kickoff)
           champdt   (dtfmt/parse (dtfmt/formatters :date-hour-minute-second) frcchampionship)
@@ -43,17 +50,70 @@
       (jdbc/insert! db-spec :season season-rec)
       season-rec)))
 
-(def team-listing (atom {}))
+(defn insert-team
+  [team-map]
+  (if-not (seq (jdbc/query db-spec ["select teamnumber from team where teamnumber = ?" (:teamnumber team-map)]))
+    (jdbc/insert! db-spec :team team-map)
+    ; TODO: (jdbc/update! db-spec :team team-map)
+    ))
 
-;(defn team-list
-;  [server year]
-;  (let [page-range (map inc (range (/ (:teamCount year-info) 50)))
-;        base-url (str (build-url server api-root "teams" year) "?page=")
-;        tlist []
-;        full-list (flatten (map #(cons tlist (:teams (parse-json (:body (client/get (str base-url %) {:debug true :headers { :authorization auth-token }}))))) page-range))]
-;    (swap! (map 
+(defn fetch-teams-api
+  []
+  (let [season     (get-season-info)
+        page-range (map inc (range (/ (:teamcount season) 50)))
+        base-url   (str (build-url server api-root "teams" current-year) "?page=")]
+    (flatten (map #(:teams (parse-json (:body (client/get (str base-url %) std-header)))) page-range))))
 
-;
+(defn initial-team-load
+  []
+  (let [teams (fetch-teams-api)]
+    (doseq [team teams] (insert-team team))))
+
+(defn fetch-events-api
+  "/api/v1.0/events/{season}{?eventCode}{?teamNumber}{&districtCode}{&excludeDistrict}"
+  []
+  (let [season (get-season-info)]
+    (parse-json (:body (client/get (build-url server api-root "events" current-year) std-header)))))
+
+(defn initial-event-load
+  [events]
+  (let [insertfn (fn insert-event [event]
+                   (if-not (seq (jdbc/query db-spec ["select code from event where code = ?" (:code event)]))
+                     (jdbc/insert! db-spec :event event)))]
+    (map insertfn (:events events))))
+
+
+(defn fetch-event-ranking
+  "/api/v1.0/rankings/{season}/{eventCode}{?teamNumber}{&top}"
+  [event-code]
+  (let [season   (get-season-info)
+        url      (build-url server api-root "rankings" current-year event-code)
+        rankings (:rankings (parse-json (:body (client/get url std-header))))]
+    (map #(assoc % :event_code event-code) rankings)))
+
+(defn insert-rankings
+  [rankings]
+  (let [insertfn (fn insert-ranking [ranking] 
+                   (if-not (seq (jdbc/query db-spec 
+                                            ["select event_code, teamnumber from event_rank where event_code = ? and teamnumber = ?" 
+                                             (:event_code ranking) (:teamnumber ranking)]))
+                     (jdbc/insert! db-spec :event_rank ranking)))]
+    (doseq [ranking rankings] (insertfn ranking))))
+
+(defn initial-ranking-load
+  []
+  (let [eventcds (jdbc/query db-spec "select code from event where dateEnd < now()")]
+    (doseq [{:keys [code]} eventcds] (insert-rankings (fetch-event-ranking code)))))
+
+
+(defn init-database
+  []
+  (do
+    (get-season-info)
+    (initial-team-load)
+    (initial-event-load (fetch-events-api))
+    (initial-ranking-load)))
+
 ;(defn team-ranking 
 ;  [server year]
 ;  (let [response (client/get (build-url server api-root server year) {:debug true :headers { :authorization auth-token }})]))
@@ -66,10 +126,11 @@
 ;
 ;(defn -main 
 ;  [& args]
-;  (let [page (frrank staging-server 2015)]
+;  (let [page (frrank server 2015)]
 ;    (println page)
 ;    (spit "out.html" page)))
 ;
 ;
 ;;(defroutes routes
 ;  ;GET "/" [] (team-ranking))
+
--- src/frrank/resources/ddl.sql
+++ src/frrank/resources/ddl.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE IF NOT EXISTS frrank;
 use frrank;
 
-CREATE TABLE IF NOT EXISTS teams (
+CREATE TABLE IF NOT EXISTS team (
     teamNumber integer primary key,
     rookieYear integer,
-    stateProv varchar(10),
+    stateProv varchar(128),
     robotName varchar(256),
     city varchar(50),
     nameFull varchar(1024),
@@ -21,4 +21,36 @@ CREATE TABLE IF NOT EXISTS season (
     kickoff datetime,
     rookieStart integer,
     teamCount INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS event (
+    code VARCHAR(128) PRIMARY KEY, -- alphanumeric event code unique to the event, and used to make additional api calls regarding the specific event
+    divisionCode VARCHAR(128),     -- alphanumeric event code of the parent event, if the event is the child of another
+    name VARCHAR(256),             -- official name of the event
+    type VARCHAR(32),              -- (regional/districtEvent/districtChampionship/championship) type of event
+    districtCode VARCHAR(32),      -- if district event or district championship, the code of the associated district
+    venue VARCHAR(256),            -- name of the venue
+    city VARCHAR(128),             -- city of event
+    stateProv VARCHAR(128),        -- state of event
+    country VARCHAR(128),          -- country code of event
+    dateStart DATETIME,            -- scheduled start date of the event
+    dateEnd DATETIME               -- scheduled end date of the event
+);
+
+CREATE TABLE IF NOT EXISTS event_rank (
+    event_code VARCHAR(32),     -- This is not provided as part of the api response, because it is part of the request
+    teamNumber INTEGER,         -- team number of the team in the record
+    autoPoints INTEGER,         -- total of all autonomous points the team has accumulated in the qualification rounds of the event
+    containerPoints INTEGER,    -- total number of container points the team has accumulated in the qualification rounds of the event
+    coopertitionPoints INTEGER, -- total number of Coopertition points the team has accumulated in the qualification rounds of the event
+    dq INTEGER,                 -- total number of times the team has been disqualified from a match in the qualification rounds of the event
+    litterPoints INTEGER,       -- total number of litter points the team has accumulated in the qualification rounds of the event (sum of all three types of litter points)
+    matchesPlayed INTEGER,      -- total number of match the team has played in the qualification rounds of the event
+    qualAverage FLOAT,          -- total qualification average (qa) of the team in the qualification rounds of the event (total points scored in all matches, divided by number of matches). Value is truncated in accordance with the Game Manual
+    rank INTEGER,               -- current rank (1-n) of the team in the qualification rounds of the event, where n is the number of teams at the event
+    totePoints INTEGER,         -- total number of tote points the team has accumulated in the qualification rounds of the event
+    wins INTEGER,               -- not active in the 2015 season. total number of matches the team has won in the qualification rounds of the event
+    losses INTEGER,             -- not active in the 2015 season. total number of matches the team has lost in the qualification rounds of the event
+    ties INTEGER,               -- not active in the 2015 season. total number of matches the team has tied in the qualification rounds of the event
+    PRIMARY KEY (event_code, teamNumber)
 );
